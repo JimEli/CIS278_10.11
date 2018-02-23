55c2178458716ebd86ea0011f295f1f1ac05d613
Jim Eli <31859133+JimEli@users.noreply.github.com>
2018-02-23 13:43:57 -0700

    Update CIS278_Week12_10.11.cpp

--- CIS278_Week12_10.11/CIS278_Week12_10.11.cpp
+++ CIS278_Week12_10.11/CIS278_Week12_10.11.cpp
@@ -5,8 +5,11 @@
 * Date: 2/18/2018
 *
 * Exercise 10.11 on page 495-496, Polynomial Class
+*
+* This is a short demonstration program of the polynomial class.
+* 
 * Develop class Polynomial. The internal representation of a Polynomial is 
-* an array of terms. Each term contains a coefficient and an exponent—e.g., 
+* an array of terms. Each term contains a coefficient and an exponentâ€”e.g., 
 * the term
 *   2x^4
 * Has the coefficient 2 and the exponent 4. Develop a complete class 
@@ -31,32 +34,35 @@
 *   02/18/2018: Initial release. JME
 *************************************************************************/
 #include <iostream>  // cout/endl
-
 #include "polynomial.h" // Our polynomial class.
 
 using std::cout;
 using std::endl;
 
-// Perform basic testing of Polynomial class.
+// Perform basic demonstration of Polynomial class.
 int main()
 {
-	// Create polynomial 7x^4 - x^2 + 3.
+	// Create a polynomial, 7x^4 - x^2 + 3.
 	Polynomial a;
 	a.setTerm(4, 7);
 	a.setTerm(2, -1);
 	a.setTerm(0, 3);
-	cout << "degree of (" << a << ") is: " << a.getDegree() << endl;
-	//cout << "(" << a << ") evaluated at 2 = " << a.evaluate(2) << endl;
-	//cout << "Differentiate (" << a << ") = " << a.differentiate() << endl;
 
-	// Create polynomial 6x^3 - 3x^2 + 4 with list initalizer.
-	Polynomial b({ { 3, 6 }, { 2, -3 }, { 0, 4 } });
+	// Create another polynomial, 6x^3 - 3x^2 + 4 with list initalizer.
+	Polynomial b({ { 2, -3 },{ 0, 4 } });
+
+	// Output some information about polynomials.
+	cout << "degree of (" << a << ") is: " << a.getDegree() << endl;
 	// Get a term's coefficient.
 	double c;
 	if (b.getTerm(3, c))
 		cout << "Coefficient of 3rd term of (" << b << ") is: " << c << endl;
 
-	// Add, subtract and multiply binat operators.
+	// These two methods not fully tested.
+	//cout << "(" << a << ") evaluated at 2 = " << a.evaluate(2) << endl;
+	//cout << "Differentiate (" << a << ") = " << a.differentiate() << endl;
+
+	// Add, subtract and multiply binary operators.
 	cout << "(" << a << ") + (" << b << ") = " << (a + b) << endl;
 	cout << "(" << a << ") - (" << b << ") = " << (a - b) << endl;
 	cout << "(" << a << ") * (" << b << ") = " << (a * b) << endl;
