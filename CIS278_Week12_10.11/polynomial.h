e22f693c332c87f3bbc6b86a46418ccb3f63726b
Jim Eli <31859133+JimEli@users.noreply.github.com>
2018-04-12 10:39:58 -0700

    Update polynomial.h

--- CIS278_Week12_10.11/polynomial.h
+++ CIS278_Week12_10.11/polynomial.h
@@ -5,9 +5,10 @@
 * Date: 2/18/2018
 *
 * Exercise 10.11 on page 495-496, Polynomial Class
+*
 * Develop class Polynomial. The internal representation of a Polynomial is
 * an array of terms. Each term contains a coefficient and an exponent—e.g.,
-* the term
+* the term:
 *   2x^4
 * Has the coefficient 2 and the exponent 4. Develop a complete class
 * containing proper constructor and destructor functions as well as set and
@@ -22,13 +23,16 @@
 *
 * Notes:
 *  (1) Degree, unary minus, stream and untested evaluate and differentiate
-*      methods added also.
+*      methods added.
 *  (2) Compiled with MS Visual Studio 2017 Community (v141).
 *
 * Submitted in partial fulfillment of the requirements of PCC CIS-278.
 *************************************************************************
 * Change Log:
 *   02/18/2018: Initial release. JME
+*   04/03/2018: Divede into .h & .cpp files. JME
+*               Changed return value from getDegree() to unsigned. JME
+*   04/12/2018: Added boost test file. JME
 *************************************************************************/
 #include <iostream>  // cout/endl
 #include <string>    // string
@@ -42,110 +46,45 @@
 
 #include "range_for_reverse_iterator.h"
 
+const double EVALUATION_CONSTANT{ 1.1 };
+
 class Polynomial
 {
 private:
-	// Polynomial terms in format of map<exponent, coefficient>.
+	// Polynomial terms contained in a map in the 
+	// format of map<exponent, coefficient>.
 	std::map<unsigned, double> terms;
 
 	// degree of polynomial (0 for the zero polynomial).
 	unsigned degree;
 
 	// Determines if a term exists for exponent.
-	bool exists(const unsigned& exponent)
-	{
-		return terms.count(exponent);
-	}
+	bool exists(const unsigned& exponent) const;
 
 public:
 
 	// Default constructor.
-	Polynomial()
-	{
-		// Empty polynomial.
-		terms.clear();
-		// Initialize as null polynomial.
-		setTerm(0, 0);
-		// No degree.
-		degree = 0;
-	}
-
+	Polynomial();
 	// List initaializer constructor.
-	Polynomial(std::initializer_list<std::pair<const unsigned, double>> init) : terms(init)
-	{
-		// Init empty 0 term in polynomial.
-		if (!exists(0))
-			setTerm(0, 0.);
-	}
-
+	Polynomial(std::initializer_list<std::pair<const unsigned, double>> init);
 	// Default destructor.
 	~Polynomial() = default;
 
 	// Setter function for term.
-	void setTerm(unsigned exponent, double coefficient)
-	{
-		// Set or update an existing polynomial term.
-		terms[exponent] += coefficient;
-
-		// Update polynomial degree.
-		if (exponent > degree)
-			degree = exponent;
-	}
-
+	void setTerm(const unsigned exponent, const double coefficient);
 	// Getter function for term coefficient.
-	bool getTerm(unsigned exponent, double& coefficient)
-	{
-		// Get polynomial term if exists.
-		if (exists(exponent))
-		{
-			coefficient = terms[exponent];
-			return true;
-		}
-
-		return false;
-	}
-
+	bool const getTerm(const unsigned exponent, double& coefficient);
 	// Getter function for polynomial degree.
-	int getDegree()
-	{
-		return terms.rbegin()->first;
-	}
-/*
+	unsigned getDegree() const;
+
 	// Uses Horner's method to calculate the polynomial evaluated at x.
-	double evaluate(double x)
-	{
-		double p{ 0. };
-
-		for (int i = degree; i >= 0; i--)
-			if (exists(i))
-				p = terms[i] + (x * p);
-			else
-				p = (x * p);
-		// Old school method.
-		//for (auto t : terms) p += (pow(x, t.first) * t.second);
-
-		return p;
-	}
-
+	double evaluate(double x);
 	// Differentiate polynomial and return result.
-	Polynomial differentiate()
-	{
-		Polynomial derivative;
+	Polynomial differentiate();
 
-		if (degree == 0)
-			return derivative;
-
-		derivative.degree = degree - 1;
-		// Calculate looping through all terms.
-		for (unsigned i = 0; i < degree; i++)
-			if (exists(i + 1))
-				derivative.terms[i] = (i + 1) * terms[i + 1];
-
-		return derivative;
-	}
-*/
 	// Overload assignment operator.
-	Polynomial& operator= (const Polynomial rhs) {
+	const Polynomial& operator= (const Polynomial rhs)
+	{
 		// Check for self-assignment.
 		if (this == &rhs)
 			// Skip assignment, and just return *this.
@@ -157,14 +96,14 @@ public:
 
 		for (auto t : rhs.terms)
 			setTerm(t.first, t.second);
-		
+
 		degree = rhs.degree;
 
 		return *this;
 	}
 
 	// Add polynomials via overloaded binary plus operator.
-	Polynomial operator+ (Polynomial rhs)
+	const Polynomial operator+ (const Polynomial rhs)
 	{
 		// Copy this's terms to result poly.
 		Polynomial result = *this;
@@ -172,7 +111,9 @@ public:
 		// Use accumulator to add rhs to lhs terms. 
 		result.terms = accumulate(rhs.terms.cbegin(), rhs.terms.cend(), result.terms,
 			[](std::map<unsigned, double>& t, const std::pair<const unsigned, double>& p)
-			{ return (t[p.first] += p.second, t); }
+			{ 
+				return (t[p.first] += p.second, t); 
+			}
 		);
 
 		// Set poly result's degree.
@@ -182,7 +123,7 @@ public:
 	}
 
 	// Subtract polynomials via overloaded binary minus operator.
-	Polynomial operator- (Polynomial rhs)
+	const Polynomial operator- (const Polynomial rhs)
 	{
 		// Copy this's terms to result poly.
 		Polynomial result = *this;
@@ -190,7 +131,9 @@ public:
 		// Use accumulator to subtract rhs from lhs terms. 
 		result.terms = accumulate(rhs.terms.cbegin(), rhs.terms.cend(), result.terms,
 			[](std::map<unsigned, double> &t, const std::pair<const unsigned, double> &p)
-			{ return (t[p.first] -= p.second, t); }
+			{ 
+				return (t[p.first] -= p.second, t); 
+			}
 		);
 
 		// Set poly result's degree.
@@ -200,16 +143,18 @@ public:
 	}
 
 	// Multiply Polynomials via overloaded binary multiplication operator.
-	Polynomial operator* (Polynomial rhs)
+	const Polynomial operator* (const Polynomial rhs)
 	{
 		Polynomial result;
 
 		// Multiply all lhs (or this) terms by all rhs terms.
-		for_each(this->terms.cbegin(), this->terms.cend(), [&result, rhs](auto lhsTerm){
-			for_each(rhs.terms.cbegin(), rhs.terms.cend(), [&result, lhsTerm](auto rhsTerm){
+		for_each(this->terms.cbegin(), this->terms.cend(), [&result, rhs](auto lhsTerm) 
+		{
+			for_each(rhs.terms.cbegin(), rhs.terms.cend(), [&result, lhsTerm](auto rhsTerm) 
+			{
 				result.terms[lhsTerm.first + rhsTerm.first] += (lhsTerm.second * rhsTerm.second);
-			});
-		});
+			} );
+		} );
 
 		// Set polynomial result's degree.
 		result.degree = result.getDegree();
@@ -217,29 +162,29 @@ public:
 		return result;
 	}
 
-	// Overloaded unary += operator, passes to our "overloaded +".
-	Polynomial& operator+= (Polynomial p)
+	// Overloaded unary += operator, passes our to "overloaded +".
+	const Polynomial& operator+= (const Polynomial p)
 	{
 		*this = *this + p;
 		return *this;
 	}
 
-	// Overloaded unary -= operator, passes to our "overloaded -".
-	Polynomial& operator-= (Polynomial p)
+	// Overloaded unary -= operator, passes our to "overloaded -".
+	const Polynomial& operator-= (const Polynomial p)
 	{
 		*this = *this - p;
 		return *this;
 	}
 
 	// Overloaded unary *= operator, passes to our "overloaded *".
-	Polynomial& operator*= (const Polynomial p)
+	const Polynomial& operator*= (const Polynomial p)
 	{
 		*this = *this * p;
 		return *this;
 	}
 
 	// Overloaded unary minus operator.
-	Polynomial operator- ()
+	const Polynomial operator- ()
 	{
 		Polynomial p = *this;
 
@@ -248,6 +193,71 @@ public:
 			t.second = -t.second;
 
 		return p;
+	}
+
+	// Overload equality operator.
+	const bool operator== (Polynomial rhs)
+	{
+		Polynomial p = *this;
+
+		if (p.getDegree() != rhs.getDegree())
+			return false;
+
+		// Iterate through all lhs terms.
+		for (auto& t : p.terms)
+		{
+			double c;
+
+			// Check if rhs term exists.
+			if (rhs.getTerm(t.first, c))
+				// If coefficients don't match then fail.
+				if (t.second != c)
+					return false;
+		}
+
+		return true;
+	}
+
+	// Overload inequality operator.
+	const bool operator!= (Polynomial rhs)
+	{
+		return !(*this == rhs);
+	}
+
+	// Overload greater than operator.
+	const bool operator> (Polynomial rhs)
+	{
+		if (evaluate(EVALUATION_CONSTANT) > rhs.evaluate(EVALUATION_CONSTANT))
+			return true;
+
+		return false;
+	}
+
+	// Overload greater than or equal operator.
+	const bool operator>= (Polynomial rhs)
+	{
+		if (evaluate(EVALUATION_CONSTANT) >= rhs.evaluate(EVALUATION_CONSTANT))
+			return true;
+
+		return false;
+	}
+
+	// Overload less than operator.
+	const bool operator< (Polynomial rhs)
+	{
+		if (evaluate(EVALUATION_CONSTANT) < rhs.evaluate(EVALUATION_CONSTANT))
+			return true;
+
+		return false;
+	}
+
+	// Overload less than or equal operator.
+	const bool operator<= (Polynomial rhs)
+	{
+		if (evaluate(EVALUATION_CONSTANT) <= rhs.evaluate(EVALUATION_CONSTANT))
+			return true;
+
+		return false;
 	}
 
 	// Stream polynomial.
@@ -278,3 +288,47 @@ public:
 		return os << s;
 	}
 };
+
+/*
+void Polynomial::Shift(size_t shift) {
+	if (shift == 0) 
+		return;
+
+	vector<double> temp(shift);
+	for (int i = 0; i <= degree; ++i) 
+		temp.push_back(coefficients[i]);
+
+	coefficients = temp;
+	degree += shift;
+}
+
+Polynomial operator/ (const Polynomial& lhs, const Polynomial& rhs)
+{
+	Polynomial quotient;
+	Polynomial mod;
+	mod = *this;
+	size_t my_degree = mod.getDegree();
+	size_t rhs_degree = rhs.getDegree();
+
+	if (rhs_degree == 0 && rhs[0] == 0) 
+		throw std::overflow_error("Divide by zero");
+
+	if (my_degree < rhs_degree) {
+		quotient = Polynomial();
+		mod = rhs;
+		return;
+	}
+
+	while (mod.getDegree() >= rhs_degree && !(mod[0] == 0 && mod.getDegree() == 0)) {
+		double coef = mod[mod.getDegree()] / rhs[rhs_degree];
+		if (coef == 0)
+			break;
+		unsigned degree_diff = mod.getDegree() - rhs_degree;
+		Polynomial temp(coef);
+		temp.Shift(degree_diff);
+		quotient += temp;
+		mod -= temp * rhs;
+	}
+	return quotient;
+}
+*/
